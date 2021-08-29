4a9f1f66e8d9db01c7ef8070d7e1e1c0f34b44af
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-29 13:50:11 +0100

    environs imported

--- resumeapp/settings.py
+++ resumeapp/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
