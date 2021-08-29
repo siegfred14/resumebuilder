25913a0c14e0c113aa7288b597396734f4b4c79f
Siegfred Samson <siegfredsamson@gmail.com>
2021-08-29 13:28:33 +0100

    models updated for images

--- cvdoc/models.py
+++ cvdoc/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class Person(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField(max_length=20)
+    email = models.EmailField(max_length=50)
+    highschool = models.CharField(max_length=70)
+    degree = models.CharField(max_length=50)
+    university = models.CharField(max_length=100)
+    skill = models.TextField(max_length=1000)
+    about_you = models.TextField(max_length=1000)
+    previous_work = models.TextField(max_length=1000)
