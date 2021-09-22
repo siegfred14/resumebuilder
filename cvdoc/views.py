93d27af01888f18764fb0920965466bd982cb8a0
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-22 21:49:56 +0100

    views.py modified

--- cvdoc/views.py
+++ cvdoc/views.py
@@ -1,26 +1,26 @@
 from django.http import request
 from django.shortcuts import render
 from . import models
-from templates import *
+# from templates import *
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def resume(request):
-    if request.method == "POST":
-        person = models.Person
-        image = request.POST.get('image')
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-        highschool = request.POST.get('highschool')
-        degree = request.POST.get('degree')
-        university = request.POST.get('university')
-        skill = request.POST.get('skill')
-        about_you = request.POST.get('about_you')
-        previous_work = request.POST.get('previous_work')
+    # if request.method == "POST":
+    #     person = models.Person
+    #     image = request.POST.get('image')
+    #     name = request.POST.get('name')
+    #     phone = request.POST.get('phone')
+    #     email = request.POST.get('email')
+    #     highschool = request.POST.get('highschool')
+    #     degree = request.POST.get('degree')
+    #     university = request.POST.get('university')
+    #     skill = request.POST.get('skill')
+    #     about_you = request.POST.get('about_you')
+    #     previous_work = request.POST.get('previous_work')
 
-        return render(request, 'accept.html')
+    return render(request, 'accept.html')
 
-    return HttpResponse('Please Enter Correct Details', content_type="text/plain")
+    # return HttpResponse('Please Enter Correct Details', content_type="text/plain")
