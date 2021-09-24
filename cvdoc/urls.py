c0dc0206a5d45d67c986763a4aceaee8fa66f210
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-24 21:54:35 +0100

    staticfiles_urlpatterns imported in urls.py

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from .views import resume
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', resume, name="resume"),
