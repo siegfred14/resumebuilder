aa486d6194b2ae11cbdfc3da6b251ce9300f6fc5
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-25 23:26:34 +0100

    fields added in views.py

--- cvdoc/views.py
+++ cvdoc/views.py
@@ -1,6 +1,6 @@
 from django.http import request
 from django.shortcuts import render
-from . import models
+from .models import Person
 # from templates import *
 from django.http import HttpResponse
 
@@ -8,18 +8,19 @@ from django.http import HttpResponse
 
 
 def resume(request):
-    # if request.method == "POST":
-    #     person = models.Person
-    #     image = request.POST.get('image')
-    #     name = request.POST.get('name')
-    #     phone = request.POST.get('phone')
-    #     email = request.POST.get('email')
-    #     highschool = request.POST.get('highschool')
-    #     degree = request.POST.get('degree')
-    #     university = request.POST.get('university')
-    #     skill = request.POST.get('skill')
-    #     about_you = request.POST.get('about_you')
-    #     previous_work = request.POST.get('previous_work')
+    if request.method == "POST":
+        person = Person(image=image, name=name, phone=phone, email=email, highschool=highschool, degree=degree,
+                        university=university, skill=skill, about_you=about_you, previous_work=previous_work)
+        image = request.POST.get('image')
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        highschool = request.POST.get('highschool')
+        degree = request.POST.get('degree')
+        university = request.POST.get('university')
+        skill = request.POST.get('skill')
+        about_you = request.POST.get('about_you')
+        previous_work = request.POST.get('previous_work')
 
     return render(request, 'accept.html')
 
