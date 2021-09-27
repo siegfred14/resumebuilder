02a0644787acb4479156f75646da2f3428d2d6e6
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-27 16:02:32 +0100

    link created for pdf in urls.py

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -5,5 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', resume, name="resume"),
+    path('<int:id>/', pdf, name="pdf"),
 ]
 urlpatterns += staticfiles_urlpatterns()
