0d1a4e96f0779bf75da34229d9642d98e4f08624
Siegfred Samson <siegfredsamson@gmail.com>
2021-09-27 16:01:57 +0100

    pdf imported to urls.py

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import resume
+from .views import resume, pdf
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
