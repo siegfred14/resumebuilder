20cdc7dd378ccb40bb80ae2df3cb0e3fd09ebee0
Siegfred Samson <siegfredsamson@gmail.com>
2021-10-02 17:21:04 +0100

    list endpoint added to urls.py

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import resume, pdf
+from .views import resume, pdf, list
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', resume, name="resume"),
     path('<int:id>/', pdf, name="pdf"),
+    path('list/', list, name="list")
 ]
 urlpatterns += staticfiles_urlpatterns()
