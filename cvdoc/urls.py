3021d7acd6f32bc28b8033983e71387113717629
Siegfred Samson <siegfredsamson@gmail.com>
2021-10-21 20:31:38 +0100

    urls for resume2 created

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', resume, name="resume"),
     path('<int:id>/', pdf, name="pdf"),
-    path('list/', list, name="list")
+    path('list/', list, name="list"),
+    path('resume2/' resume2, name='resume2')
 ]
 urlpatterns += staticfiles_urlpatterns()
