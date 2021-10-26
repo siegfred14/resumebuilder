718f2f471b719531ed4d27a103963bee65cf55b8
Siegfred Samson <siegfredsamson@gmail.com>
2021-10-26 15:00:37 +0100

    urls.py updated for static files

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from .views import resume, pdf, list, resume2
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', resume, name="resume"),
     path('<int:id>/', pdf, name="pdf"),
     path('list/', list, name="list"),
     path('resume2/', resume2, name='resume2')
-]
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
