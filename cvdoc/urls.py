fec49814122e745fbf702c07d5e922ef0f48cdf0
Siegfred Samson <siegfredsamson@gmail.com>
2021-10-29 22:05:52 +0100

    home urls created

--- cvdoc/urls.py
+++ cvdoc/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', home, name="home"),
     path('resume/', resume, name="resume"),
     path('<int:id>/', pdf, name="pdf"),
     path('list/', list, name="list"),
